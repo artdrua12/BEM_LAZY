5cdcfcb705af81f08cda0b9f92bb90d6750441e9
Alex <pasechnyy@proitr.ru>
2021-12-21 18:24:43 +0300

    tab на проигрыватель

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-
+let video
 class Video {
     constructor(url) {
         this.player = this.init();
+        this.focusableElementsCollections = document.querySelectorAll('.popap__control');
+        this.focusableElements = Array.prototype.slice.call(this.focusableElementsCollections);
+        this.firstTabStop = this.focusableElements[0];
+        this.lastTabStop = this.focusableElements[this.focusableElements.length - 1];
 
-        document.addEventListener('keydown', (e) => {
-            if (e.which === 27) {
-                this.player.pauseVideo();
-                document.location.href = "#";
-            }
-        })
+        document.addEventListener('keydown', (e) => this.pressKey(e));
+        this.addListener();
     }
+
     init() {
         return new YT.Player('player', {
             width: '720',
             height: '480',
-            videoId: 'M7lc1UVf-VE',
+            videoId: 'sg8ui5vEC-o',
             playerVars: {
                 autoplay: 1,
+                showinfo: 0,
+                modestbranding: 0,
+                controls: 0
             }
         })
+    }
+    addListener(){
+        this.focusableElementsCollections[0].addEventListener('click',()=>{
+           this.player.playVideo();
+        })
+        this.focusableElementsCollections[1].addEventListener('click',()=>{
+            this.player.stopVideo();
+         })
+         this.focusableElementsCollections[2].addEventListener('click',()=>{
+            this.player.pauseVideo();
+         })
+    }
+
+    pressKey(e) {
+        // ESCAPE
+        if (e.code === 'Escape') {
+            this.player.pauseVideo();
+            document.location.href = "#";
+        }
+
+        if (e.code === 'Tab') {
+            // SHIFT + TAB
+            if (e.shiftKey) {
+                if (document.activeElement === this.firstTabStop) {
+                    e.preventDefault();
+                    this.lastTabStop.focus();
+                }
+
+                // TAB
+            } else {
+                if (document.activeElement === this.lastTabStop) {
+                    e.preventDefault();
+                    this.firstTabStop.focus();
+                }
+            }
+        }
     }
 
     pause() {
@@ -28,5 +68,8 @@ class Video {
 
 function play() {
     video = new Video('play');
+}
+function pause() {
+    video.pause();
 }
 
